496b3aac65c1b13968c945fd442abb8d27b2c364
125BCT066 <uddhabkshrestha@gmail.com>
2013-08-19 11:48:12 +0545

    Service Interface and classes
    Renamed default interface and class
    Changed gitignore back

--- Last Release/Service/WCFServiceSurferlite/ServiceSurferlite.svc.cs
+++ Last Release/Service/WCFServiceSurferlite/ServiceSurferlite.svc.cs
@@ -8,9 +8,9 @@ using System.Text;
 
 namespace WCFServiceSurferlite
 {
-    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
-    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
-    public class Service1 : IService1
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "ServiceSurferLite" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select ServiceSurferLite.svc or ServiceSurferLite.svc.cs at the Solution Explorer and start debugging.
+    public class ServiceSurferLite : IServiceSurferlite
     {
         public string GetData(int value)
         {
