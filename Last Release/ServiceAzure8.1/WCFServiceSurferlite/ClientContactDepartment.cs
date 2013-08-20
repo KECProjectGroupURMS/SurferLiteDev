bf4a5afcaf2fcb86692223e530a3af219393f8dd
125BCT066 <uddhabkshrestha@gmail.com>
2013-08-20 22:40:38 +0545

    Added the project Azure service 8.1
    The project was created as reference to service 8.

---
+++ Last Release/ServiceAzure8.1/WCFServiceSurferlite/ClientContactDepartment.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WCFServiceSurferlite
+{
+    public class ClientContactDepartment
+    {
+        public InternetContactDepartment InternetContactDepartment
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+    }
+}
