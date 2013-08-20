b30e4aaa2b97f30ea2a8f22e370e004881ff681e
125BCT066 <uddhabkshrestha@gmail.com>
2013-08-20 22:27:09 +0545

    implementation initiation

--- Last Release/Client 8.1/Client81/CustomerDepartment.cs
+++ Last Release/Client 8.1/Client81/CustomerDepartment.cs
@@ -9,6 +9,28 @@ namespace Client81
     // This class gets and queue request from users and also give data in response to each request.
     class CustomerDepartment
     {
+        private string status;
+        private string url;
+
+        public string stringURL
+        {
+            get
+            {
+                return url;
+            }
+            set
+            {
+                if (value.Contains("http://"))
+                {
+                    url = value;
+                }
+                else
+                {
+                    url = "http://" + value;
+                }
+            }
+        }
+
         internal CallerDepartment CallerDepartment
         {
             get
@@ -31,46 +53,9 @@ namespace Client81
             }
         }
 
-        /// <summary>
-        /// This method is async method and is used to display page through SurferLite Service
-        /// </summary>
-        /// <param name="URLString">The URL in string which you want to display in WebViewBrowse</param>
-        private async void NavigateThroughSurferLite(string URLString)
+        private Uri UrlModifier(string URLString)
         {
-            try
-            {
-                Uri URL = new Uri(URLString);
-                //ProgressRingLoad.IsActive = true;
-
-                //Although Service Contract says GetHtml(URL) returns stream. It automatically is byte[].
-                //XXXXXX: Stream pullStream = await client.GetHtmlAsync(URL);
-
-                //byte[] pullStream = await client.GetHtmlAsync(URL);
-
-                // Update total data
-                //dataSize += pullStream.Length;
-
-                // Displaying size of byte[] got from service
-                //TextBlockSize.Text = pullStream.Length.ToString() + " B";
-
-                // Decompressing pullStream to get actual decompressed data
-                //string result = CompressionDepartment.DecompressBytes(pullStream);
-
-                // To view in Html Source
-                //TextBlockSource.Text = result;
-
-                // result is ready and is displayed in webview
-                //WebViewBrowse.NavigateToString(result);
-            }
-
-            catch (Exception e)
-            {
-                // There is some error. This needs modification so Exception is exact
-                //****WebViewBrowse.NavigateToString(e.ToString());
-            }
-
-            //*** ProgressRingLoad.IsActive = false;
-
+            return new Uri("https://www.google.com/");
         }
 
     }
